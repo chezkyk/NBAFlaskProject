41957c3fdae68f541de03c6b9ac0d12bb86132ac
chezkyk <Y0548532123@GMAIL.COM>
2024-09-19 13:10:56 +0300

    update player model with playerId filed, added repr to print the players name.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from db import db
+from flask_migrate import Migrate
+from blueprints.player import player_bp
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nba.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+migrate = Migrate(app, db)
+app.register_blueprint(player_bp)
+
+if __name__ == '__main__':
+    app.run()
