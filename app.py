bc17cd9c96425864e1ae3c0b37709c2e0c44829d
chezkyk <Y0548532123@GMAIL.COM>
2024-09-19 17:51:06 +0300

    Added a blue print for group

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from db import db
 from flask_migrate import Migrate
 from blueprints.player import player_bp
+from blueprints.group import group_bp
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nba.db'
@@ -14,6 +15,6 @@ with app.app_context():
 
 migrate = Migrate(app, db)
 app.register_blueprint(player_bp)
-
+app.register_blueprint(group_bp)
 if __name__ == '__main__':
     app.run()
